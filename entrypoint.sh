d3f4a6e1cd374fb9a39677e0cf575d6af3f19dc3
Aryeh Harris <harryttd@gmail.com>
2023-07-25 16:43:20 -0400

    Cleanup packages

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
-# We need to buid because the configuration file may be modified at runtime.
 
+# We need to buid because the configuration file may be modified at runtime.
 npm run build
 
 serve build --single -p 8080 --no-port-switching
