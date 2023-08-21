21494a51a32018991dda2bc109387e16f98a908d
Aryeh Harris <harryttd@gmail.com>
2023-08-21 19:25:36 -0400

    Add OxheadAlpha logo and links to header and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,62 @@
+import { Github, Twitter, Medium } from "react-bootstrap-icons"
+import { Container, Row, Col } from "react-bootstrap"
+
 const { APP_DESCRIPTION, APP_VERSION } = import.meta.env
 
 function Footer() {
   return (
     <footer className="footer py-3 bg-light">
-      <span className="text-muted">
-        {APP_DESCRIPTION} - v{APP_VERSION}
-      </span>
+      <Container>
+        <Row>
+          <Col sm={3} >
+            <span className="text-muted">
+              {APP_DESCRIPTION} - v{APP_VERSION}
+            </span>
+          </Col>
+          <Col sm={9}  className="d-flex justify-content-end align-items-center">
+            <a
+              href="https://www.oxheadalpha.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mx-2 text-muted d-flex align-items-center no-underline"
+              style={{ textDecoration: "none" }}
+            >
+              <img
+                src="/public/oxheadalpha.svg"
+                alt="Oxhead Alpha Logo"
+                height="24"
+                className="mr-2"
+              />
+              <span className="logotext_oxhead">Oxhead</span>{" "}
+              <span className="logotext_alpha">Alpha</span>
+            </a>
+            <a
+              href="https://github.com/oxheadalpha"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mx-2 text-muted"
+            >
+              <Github size={24} />
+            </a>
+            <a
+              href="https://medium.com/the-aleph"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mx-2 text-muted"
+            >
+              <Medium size={24} />
+            </a>
+            <a
+              href="https://twitter.com/oxheadalpha"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mx-2 text-muted"
+            >
+              <Twitter size={24} />
+            </a>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,33 @@
-import { Nav, Navbar, Container } from 'react-bootstrap';
-import { Github, Twitter } from 'react-bootstrap-icons';
-import Config from "../Config";
+import { Nav, Navbar, Container } from "react-bootstrap"
+import { Github } from "react-bootstrap-icons"
+import Config from "../Config"
 
 function Header() {
-    return (
-        <Navbar bg="light" expand="lg">
-            <Container fluid>
-                <Nav>
-                    <Navbar.Brand>
-                        {Config.application.name}
-                    </Navbar.Brand>
-                </Nav>
-                <Nav>
-
-                    <Nav.Link href={Config.application.githubRepo} target="_blank">
-                        <Github />
-                    </Nav.Link>
-                </Nav>
-
-            </Container>
-        </Navbar>
-    )
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Nav>
+          <Navbar.Brand>
+            <a href="https://www.oxheadalpha.com" target="_blank">
+              <img
+                src="/public/oxheadalpha.svg"
+                className="d-inline-block align-top"
+                width="30"
+                height="30"
+                alt="Oxhead Alpha Logo"
+              />
+            </a>{" "}
+            {Config.application.name}
+          </Navbar.Brand>
+        </Nav>
+        <Nav>
+          <Nav.Link href={Config.application.githubRepo} target="_blank">
+            <Github />
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  )
 }
 
-export default Header;
+export default Header
