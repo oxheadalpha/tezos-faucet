394a762259a3ae61c00d14e65d06d662b5c2f76a
Aryeh Harris <harryttd@gmail.com>
2023-08-22 16:01:31 -0400

    Add info about pow + captcha z-index + footer fixes

--- src/components/Faucet/SplittedFaucet.tsx
+++ src/components/Faucet/SplittedFaucet.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect} from "react"
+import { useState, useEffect } from "react"
 import { TezosToolkit } from "@taquito/taquito"
-import { Alert, Card, Col, Row, Button, Spinner } from "react-bootstrap"
+import { Alert, Card, Col, Row, Button, Spinner, Form } from "react-bootstrap"
 import Parser from "html-react-parser"
 import FaucetToWalletRequest from "./FaucetToWalletRequest"
 import FaucetToInputRequest from "./FaucetToInputRequest"
@@ -20,8 +20,11 @@ export default function SplittedFaucet({
   const [isLoading, setLoading] = useState<boolean>(false)
   const [status, setStatus] = useState<string>("")
   const [statusType, setStatusType] = useState<string>("")
-  const [showAlert, setShowAlert] = useState(false)
+  const [showPowProgress, setShowPowProgress] = useState(false)
   const [powWorker, setPowWorker] = useState<Worker | null>(null)
+  const [showAlert, setShowAlert] = useState(
+    localStorage.getItem("showAlert") === "true"
+  )
 
   const statusContext: StatusContext = {
     isLoading,
@@ -51,12 +54,16 @@ export default function SplittedFaucet({
   }, [isLoading])
 
   useEffect(() => {
-    if (statusType && statusType !== "") setShowAlert(true)
-  }, [statusType])
+    if (statusType && statusType !== "") setShowPowProgress(true)
+    if (!showAlert) setShowAlert(false)
+  }, [statusType, showAlert])
 
   return (
-    <Card>
-      <Card.Header>{network.name} faucet</Card.Header>
+    <Card className="mb-3">
+      <Card.Header>
+        <Card.Title>{network.name} faucet</Card.Title>
+      </Card.Header>
+
       <Card.Body>
         <Row>
           <Col className="faucet-part-title">Fund your web wallet</Col>
@@ -77,10 +84,10 @@ export default function SplittedFaucet({
         <Row>
           <Col>
             <br />
-            {showAlert && status && (
+            {showPowProgress && status && (
               <Alert
                 variant={statusType}
-                onClose={() => setShowAlert(false)}
+                onClose={() => setShowPowProgress(false)}
                 dismissible={!isLoading}
               >
                 <div className="d-flex justify-content-between align-items-center">
@@ -97,7 +104,7 @@ export default function SplittedFaucet({
                             powWorker.terminate()
                             setPowWorker(null)
                             setLoading(false)
-                            setShowAlert(false)
+                            setShowPowProgress(false)
                           }
                         }}
                       >
@@ -110,6 +117,54 @@ export default function SplittedFaucet({
             )}
           </Col>
         </Row>
+        <hr />
+
+        <div className="d-flex justify-content-end">
+          {!showAlert && (
+            <Button
+              variant="info"
+              onClick={() => {
+                setShowAlert(true)
+                localStorage.setItem("showAlert", "true")
+              }}
+            >
+              Show Info
+            </Button>
+          )}
+        </div>
+
+        <Alert show={showAlert} variant="info" className="mt-3">
+          <p>
+            To ensure fair distribution of Tez, we've introduced proof of work
+            challenges. Before you receive your Tez, your browser will need to
+            solve these challenges. This is an automatic process that helps us
+            prevent abuse and ensure everyone gets their fair share.
+          </p>
+          <p>
+            The number and difficulty of these challenges depend on the amount
+            of Tez you request. The more Tez you ask for, the more challenges
+            your browser will need to solve. This means it might take a bit
+            longer to receive your Tez if you request a larger amount.
+          </p>
+          <p>
+            Don't worry, your browser will automatically solve these challenges.
+            All you need to do is wait a little while for the process to
+            complete before you receive your Tez.
+          </p>
+
+          <hr />
+          <div className="d-flex justify-content-end">
+            <Button
+              variant="outline-info"
+              onClick={() => {
+                setShowAlert(false)
+                localStorage.setItem("showAlert", "false")
+              }}
+            >
+              Hide
+            </Button>
+          </div>
+        </Alert>
       </Card.Body>
     </Card>
   )
